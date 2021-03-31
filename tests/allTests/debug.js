f1ad9122b55791e70c0d8429d89a9d869dd39f95
BuildTools <lily@l1lith.com>
2021-03-30 21:26:42 -0600

    update the __args test slightly

--- tests/allTests/debug.js
+++ tests/allTests/debug.js
@@ -6,9 +6,9 @@ const { assert, expect } = chai
 describe('Debug Behavior', () => {
   it('Allows hidden access to the input arguments', () => {
     const randomFunction = () => Math.random()
-    const store = new Jabr({ random: 12 }, { random: randomFunction })
+    const store = new Jabr({ random: 12 }, { random: randomFunction }, { strict: true })
     assert.deepEqual(store.__args, {
-      options: {},
+      options: { strict: true },
       properties: { random: randomFunction },
       valueMap: { random: 12 }
     })
