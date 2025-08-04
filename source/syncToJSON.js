c57db54d251233531db4ab0c185c9d3ae3e403c0
BuildTools <lily@l1lith.com>
2025-08-04 17:54:25 -0600

    Fix a bug with still not entirely using import syntax

--- source/syncToJSON.js
+++ source/syncToJSON.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync } = require('fs')
+import { readFileSync, writeFileSync } from 'fs'
 //const { readFile, writeFile } = require('fs/promises')
 
 function syncToJSON(store, jsonPath) {
